9c1cdee7a8f6decb2b4e8f8ad96ac260e4a38a60
vickyvigu17 <vickyvigu17@gmail.com>
2025-08-04 20:36:17 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Build React app
 cd client
 npm install
@@ -7,4 +8,8 @@ npm run build
 # Copy build files to backend static folder
 cd ..
 mkdir -p backend/static
-cp -r client/build/* backend/static
+cp -r client/build/* backend/static/
+
+# Install Python dependencies
+cd backend
+pip install -r requirements.txt
