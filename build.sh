28b1a4a11fc0433c40648047c49cbd581d0e6874
vickyvigu17 <vickyvigu17@gmail.com>
2025-08-04 20:26:39 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Build React app
+cd client
+npm install
+npm run build
+
+# Copy build files to backend static folder
+cd ..
+mkdir -p backend/static
+cp -r client/build/* backend/static
