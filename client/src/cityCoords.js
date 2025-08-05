7d90c61530b93615e60aaddcab1f37043e0045c3
vickyvigu17 <vickyvigu17@gmail.com>
2025-08-05 16:53:48 +0530

    Create cityCoords.js

---
+++ client/src/cityCoords.js
@@ -0,0 +1,13 @@
+// client/src/cityCoords.js
+export const cityCoords = {
+  "Cincinnati, OH": [-84.5120, 39.1031],
+  "Dallas, TX": [-96.7970, 32.7767],
+  "Atlanta, GA": [-84.3880, 33.7490],
+  "Denver, CO": [-104.9903, 39.7392],
+  "Chicago, IL": [-87.6298, 41.8781],
+  "Phoenix, AZ": [-112.0740, 33.4484],
+  "Seattle, WA": [-122.3321, 47.6062],
+  "Orlando, FL": [-81.3792, 28.5383],
+  "Nashville, TN": [-86.7816, 36.1627],
+  "Salt Lake City, UT": [-111.8910, 40.7608]
+};
